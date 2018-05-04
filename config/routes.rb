1dae06e9359fbf559f1b95b6e7ac34cc972df4a6
Cody Higgins <cody.higginsdev18@gmail.com>
2018-05-04 17:58:42 -0600

    added routes and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: "pages#home"
 
   get 'pages/about'
+
+  get '/contact', to: 'pages#contact' 
 
   resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
